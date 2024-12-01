665fc012944ed8a0cd7a0a7dcd537d8b8a1a82e7
sweetbambie <sophia@miao.fm>
2024-12-01 00:39:34 -0500

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,68 +3,61 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-<div class="header">
+  <div class="header">
     <nav>
-        <img src="/src/assets/transparentLogo.png" class="logo"/>
-            <div class="buttons">
-                <RouterLink to="/register" class="button register">Register</RouterLink>
-                <RouterLink to="/login" class="button login">Login</RouterLink>
-            </div>
-        </nav>
-        <!-- <div class="header-content">
-            <h1>Nitche Feminine Movies, Shows, and more.</h1>
-            <h3>Watch anytime and anywhere.</h3>
-            <p>Ready to watch? Type in your email to create your account.</p>
-        </div> -->
-    </div>
+      <ul class="navlist">
+        <li class="logo">Feminine Flix</li>
+        <div class="buttons">
+          <li><RouterLink to="/register" class="button register">Register</RouterLink></li>
+          <li><RouterLink to="/login" class="button login">Login</RouterLink></li>
+        </div>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
-/* html, body {
-  height: 100%;
-  margin: 0;
+.navlist {
+  list-style-type: none;
+  margin: 8px 8px;
   padding: 0;
-  width: 100%;
-}
+  overflow: hidden;
+  text-align: center;
 
-.header {
-  background-image: linear-gradient(rgba(78, 25, 20, 0.7),rgba(78,25,20,0.7)),url('/src/assets/Background.png');
-  background-position: center center;
-  background-size: cover;
-  min-width: 100%;
-  min-height: 100vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
 }
 
-.header-content {
-    font-size: 20px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    margin-top: 100px;
-} */
+.navlist li {
+  float: right;
+}
+
+.navlist li a {
+  padding: 8px 16px;
+  text-decoration: none;
+}
 
 .logo {
-  max-width: 80%;
-  margin-bottom: 20px;
+  font-size: 2rem;
+  float: left !important;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-style: italic;
+  font-weight: bold;
+  margin-left: 8px;
+  color: hotpink;
 }
 
 .buttons {
   display: flex;
-  gap: 20px;
-  margin-bottom: 50px;
 }
 
 .button {
-  padding: 10px 20px;
   background-color: rgb(255, 255, 255);
   color: black;
   border-radius: 5px;
   cursor: pointer;
+  margin: 0 4px;
+  font-weight: bold;
 }
 </style>
